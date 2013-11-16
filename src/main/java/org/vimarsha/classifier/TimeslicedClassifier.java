9cfacc7a6ba489babb0e3bf68854628b5a46689b
sunimalr <sunimalr@gmail.com>
2013-11-17 03:35:05 +0530

    TimeslicedClassifier

---
+++ src/main/java/org/vimarsha/classifier/TimeslicedClassifier.java
@@ -0,0 +1,52 @@
+package org.vimarsha.classifier;
+
+import weka.classifiers.Evaluation;
+import weka.classifiers.trees.J48;
+import weka.core.FastVector;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sunimal
+ */
+public class TimeslicedClassifier extends AbstractClassifier {
+    public TimeslicedClassifier() {
+        super();
+    }
+
+    @Override
+    public HashMap<String,String> classify(ArrayList<String> list) {
+        String[] options = new String[4];
+        options[0] = "-C";
+        options[1] = "0.25";
+        options[2] = "-M";
+        options[3] = "2";
+        J48 tree = new J48();
+        FastVector<String> predictions = null;
+
+        try {
+            tree.setOptions(options);
+            tree.buildClassifier(trainSet);
+            Evaluation eval = new Evaluation(trainSet);
+            eval.evaluateModel(tree, testSet);
+            predictions = eval.predictions();
+        } catch (Exception ex) {
+            System.out.println(ex.toString());
+        }
+
+        HashMap<String,String> functionWiseResult=new HashMap<String, String>();
+
+        if(predictions!=null){
+            int index=0;
+            for (String prediction : predictions) {
+                functionWiseResult.put(Integer.toString(index),prediction);
+                index++;
+            }
+            return functionWiseResult;
+        }else{
+            return null;
+        }
+    }
+}
