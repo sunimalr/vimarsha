0c1d3fbf9bf8441a249abf5f48fa150432bfb21b
sunimalr <sunimalr@gmail.com>
2013-11-16 21:53:15 +0530

    DataWriter interface added

---
+++ src/main/java/org/vimarsha/formatter/DataWriter.java
@@ -0,0 +1,11 @@
+package vimarsha.formatter;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sunimal
+ * Date: 11/16/13
+ * Time: 9:51 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface DataWriter {
+}
