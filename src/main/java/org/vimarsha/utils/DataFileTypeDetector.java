7e83f6034790ebb4c20026f000c84170b6b206fd
Gayashan Amarasinghe <chozrc@gmail.com>
2013-11-27 02:26:45 +0530

    DataFileTypeDetector is used to detect the data file type from a given raw file.

---
+++ src/main/java/org/vimarsha/utils/DataFileTypeDetector.java
@@ -0,0 +1,61 @@
+/*
+ *
+ *  vimarsha, Performance analysis: Machine Learning Approach
+ *  Copyright (C) 2013 vimarsha
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * /
+ */
+
+package org.vimarsha.utils;
+
+import java.io.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gayashan
+ */
+public class DataFileTypeDetector {
+    private DataFileType dataFileType;
+    private File file;
+    private BufferedReader bufferedReader;
+
+    public DataFileTypeDetector(File file) throws FileNotFoundException {
+        this.file = file;
+        this.bufferedReader = new BufferedReader(new FileReader(file));
+        this.dataFileType = DataFileType.UNKNOWN;
+    }
+
+    /***
+     * Expected header format "@datafiletype=PERF_RECORD"
+     * @return
+     * @throws IOException
+     */
+    public DataFileType getDataFileType() throws IOException {
+        String line = bufferedReader.readLine();
+        while(line.startsWith("#")){
+            line = bufferedReader.readLine();
+        }
+        line = line.trim();
+        if(line.contains("@datafiletype")){
+            if((line.split("=")[1]).equalsIgnoreCase("PERF_STAT")){
+                this.dataFileType = DataFileType.PERF_STAT;
+            } else if((line.split("=")[1]).equalsIgnoreCase("PERF_RECORD")){
+                this.dataFileType = DataFileType.PERF_RECORD;
+            }
+        }
+        this.bufferedReader.close();
+        return this.dataFileType;
+    }
+}
