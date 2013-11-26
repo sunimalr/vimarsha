4d12ab233276949cd707922d581f5d1b720a9a01
Gayashan Amarasinghe <chozrc@gmail.com>
2013-11-27 03:27:02 +0530

    Optimizations in imports.

--- src/main/java/org/vimarsha/mediator/Mediator.java
+++ src/main/java/org/vimarsha/mediator/Mediator.java
@@ -31,7 +31,6 @@ import org.xml.sax.SAXException;
 import javax.swing.table.DefaultTableModel;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 
