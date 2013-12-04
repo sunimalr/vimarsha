d1befa66d61c9c169874501f4547037934bdcc8f
Gayashan Amarasinghe <chozrc@gmail.com>
2013-12-05 02:01:06 +0530

    New comments added.

--- src/main/java/org/vimarsha/formatter/impl/PerfReportArffDataWriter.java
+++ src/main/java/org/vimarsha/formatter/impl/PerfReportArffDataWriter.java
@@ -3,6 +3,7 @@ package org.vimarsha.formatter.impl;
 import org.vimarsha.exceptions.InstructionCountNotSetException;
 import org.vimarsha.exceptions.RawEventNotFoundException;
 import org.vimarsha.exceptions.SymbolNotFoundException;
+import org.vimarsha.formatter.DataWriter;
 import org.vimarsha.formatter.OutputWriter;
 import org.vimarsha.utils.impl.PerfReportDataHolder;
 import org.vimarsha.utils.impl.PerformanceEventsHolder;
@@ -14,20 +15,40 @@ import java.math.RoundingMode;
 import java.util.ArrayList;
 
 /**
- * Created with IntelliJ IDEA.
- * User: gayashan
+ * A class which writes the parsed data from perf report to an arff file.
+ *
+ * @author gayashan
  */
 public class PerfReportArffDataWriter extends DataWriter {
     private PerfReportDataHolder perfReportDataHolder;
 
+    /**
+     * Generates a PerfReportArffDataWriter
+     *
+     * @param fileName                arff file name
+     * @param performanceEventsHolder a PerformanceEventsHolder
+     * @param perfReportDataHolder    a PerfReportDataHolder
+     * @throws IOException
+     */
     public PerfReportArffDataWriter(String fileName, PerformanceEventsHolder performanceEventsHolder, PerfReportDataHolder perfReportDataHolder) throws IOException {
         super(fileName, performanceEventsHolder);
         this.perfReportDataHolder = perfReportDataHolder;
     }
 
+    /**
+     * Writes to the arff file
+     *
+     * @throws SymbolNotFoundException   thrown when the symbol(function) is not found
+     * @throws InstructionCountNotSetException
+     *                                   thrown when instruction count is not set in the configurations
+     * @throws RawEventNotFoundException thrown when raw event is not found
+     * @throws IOException
+     */
     @Override
     public void writeToArffFile() throws SymbolNotFoundException, InstructionCountNotSetException, RawEventNotFoundException, IOException {
         int i = 1;
+        //for each event name in performanceEventsHolder check whether it is present in perfReportDataHolder
+        //if it doesn't mark it as a missing value
         for (String symbol : this.perfReportDataHolder.getSymbolsList()) {
             for (String event : this.performanceEventsHolder.getEventsHolder()) {
                 if (!this.perfReportDataHolder.getRawEventsCollection(symbol).contains(event)) {
@@ -38,41 +59,59 @@ public class PerfReportArffDataWriter extends DataWriter {
             //(not an unknown value "?")
             if (this.perfReportDataHolder.getRawEventsCollection(symbol).contains(this.performanceEventsHolder.getInstructionCountEvent()) &&
                     !(this.perfReportDataHolder.getValue(symbol, this.performanceEventsHolder.getInstructionCountEvent()).equalsIgnoreCase(PropertiesLoader.getInstance().getMissingValueIndicator()))) {
-                String out = "";
+                StringBuilder out = new StringBuilder();
+                //write a comment to the arff file the function/symbol name
                 this.arffWriter.write(PropertiesLoader.getInstance().getARFFCommentString() + System.getProperty("line.separator") +
                         PropertiesLoader.getInstance().getARFFCommentString() + i + PropertiesLoader.getInstance().getFunctionIDString() + symbol +
                         System.getProperty("line.separator") + PropertiesLoader.getInstance().getARFFCommentString() + System.getProperty("line.separator"));
+                //add that symbol to the final list of perfReportDataHolder so it can be used when function wise classification results are displayed
                 this.perfReportDataHolder.addToFinalSymbolsList(symbol);
                 ++i;
 
                 for (String event : this.performanceEventsHolder.getEventsHolder()) {
+                    //if the current value is missing
                     if (this.perfReportDataHolder.getValue(symbol, event).equalsIgnoreCase(PropertiesLoader.getInstance().getMissingValueIndicator())) {
-                        out += String.valueOf(this.perfReportDataHolder.getValue(symbol, event)) + PropertiesLoader.getInstance().getValueSeparator();
-                    } else {
-                        out += new BigDecimal(Float.parseFloat(this.perfReportDataHolder.getValue(symbol, event)) * java.lang.Math.pow(10, 9) /
-                                Float.parseFloat(this.perfReportDataHolder.getValue(symbol, this.performanceEventsHolder.getInstructionCountEvent()))).setScale(4, RoundingMode.CEILING).toPlainString();
-                        /*out += String.valueOf(Float.parseFloat(this.perfReportDataHolder.getValue(symbol,event)) * java.lang.Math.pow(10, 9) /
-                                Float.parseFloat(this.perfReportDataHolder.getValue(symbol,this.performanceEventsHolder.getInstructionCountEvent())));*/
-                        out += PropertiesLoader.getInstance().getValueSeparator();
+                        out.append(String.valueOf(this.perfReportDataHolder.getValue(symbol, event)) + PropertiesLoader.getInstance().getValueSeparator());
+                    } else { //if the value is present
+                        //calculate the normalized value = performance event count value * 10^9 / number of instructions retired
+                        out.append(new BigDecimal(Float.parseFloat(this.perfReportDataHolder.getValue(symbol, event)) * java.lang.Math.pow(10, 9) /
+                                Float.parseFloat(this.perfReportDataHolder.getValue(symbol, this.performanceEventsHolder.getInstructionCountEvent()))).setScale(4, RoundingMode.CEILING).toPlainString());
+                        out.append(PropertiesLoader.getInstance().getValueSeparator());
                     }
                 }
-                out += PropertiesLoader.getInstance().getMissingValueIndicator() + System.getProperty("line.separator");
-                this.arffWriter.write(out);
+                //add the status class - to be classified
+                out.append(PropertiesLoader.getInstance().getMissingValueIndicator() + System.getProperty("line.separator"));
+                this.arffWriter.write(out.toString());
             }
         }
         this.arffWriter.close();
     }
 
+    /**
+     * Set the output writer
+     *
+     * @param outputWriter an OutputWriter
+     */
     @Override
     public void setOutputWriter(OutputWriter outputWriter) {
         this.arffWriter = (ArffWriter) outputWriter;
     }
 
+    /**
+     * Set the PerformanceEventsHolder
+     *
+     * @param performanceEventsHolder a PerformanceEventsHolder
+     */
     @Override
     public void setEventsHolder(PerformanceEventsHolder performanceEventsHolder) {
         this.performanceEventsHolder = performanceEventsHolder;
     }
 
+    /**
+     * Returns the list of headers
+     *
+     * @return ArrayList of headers
+     */
     public ArrayList<String> getHeaders() {
         return this.headers;
     }
